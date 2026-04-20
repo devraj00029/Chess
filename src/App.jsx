bd345701304e3b30f4377124a4adaa1dc245c4c8
devraj00029 <devraj.mohapatra19991@gmail.com>
2026-04-20 12:20:12 +0530

    add: initial board state

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import createInitialBoard from "./board.js";
+
 
 const App = () =>{
-let Board = []
+  let Board = []
+  const [board,setBoard] = useState(createInitialBoard());
 
-for(let i=0;i<8;i++){
-  for(let j=0;j<8;j++){
 
-    if((i+j)%2==0){
-      Board.push(
-        <div className="bg-ink h-full w-full">
+  for(let i=0;i<8;i++){
+    for(let j=0;j<8;j++){
 
-        </div>
-      )
-    }
-    else{
-      Board.push(
-        <div className="bg-cream  h-full w-full">
+      const square = `${String.fromCharCode(97+j)}${8-i}`
 
-        </div>
-      )
+      if((i+j)%2==0){
+        Board.push(
+          <div key={square} className="bg-ink h-full w-full text-cream">
+            {board[i][j]}
+          </div>
+        )
+      }
+      else{
+        Board.push(
+          <div key={square} className="bg-cream  h-full w-full">
+            {board[i][j]}
+          </div>
+        )
+      }
     }
   }
-}
 
   return(
     <div className="flex justify-center items-center h-screen w-screen bg-ink">
---
+++ src/board.js
@@ -0,0 +1,20 @@
+
+
+const board = () =>{
+
+    const chessBoard = [
+        ["rb", "nb", "bb", "qb", "kb", "bb", "nb", "rb"], 
+        ["pb", "pb", "pb", "pb", "pb", "pb", "pb", "pb"], 
+        [null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null], 
+        ["pw", "pw", "pw", "pw", "pw", "pw", "pw", "pw"], 
+        ["rw", "nw", "bw", "qw", "kw", "bw", "nw", "rw"]  
+    ];
+
+    return chessBoard;
+
+}
+
+export default board;
